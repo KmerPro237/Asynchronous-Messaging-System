0aa6ae880cf16a0d6d305ade5a69ad8acde3ae77
KmerPro237 <fotsososthene02@gmail.com>
2023-12-20 08:55:57 +0100

    gRPC implemented

---
+++ src/main/java/gRPC/MessageBrokerClient.java
@@ -0,0 +1,87 @@
+package gRPC;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.concurrent.CountDownLatch;
+
+public class MessageBrokerClient {
+    private final ManagedChannel channel;
+    private final MessageBrokerGrpc.MessageBrokerStub stub;
+
+    public MessageBrokerClient(String host, int port) {
+        this.channel = ManagedChannelBuilder.forAddress(host, port)
+                .usePlaintext()
+                .build();
+        this.stub = MessageBrokerGrpc.newStub(channel);
+    }
+
+    public void publishMessage(String topic, String content) {
+        MessageBrokerProto.Message request = MessageBrokerProto.Message.newBuilder()
+                .setTopic(topic)
+                .setContent(content)
+                .build();
+        stub.publishMessage(request, new StreamObserver<MessageBrokerProto.Empty>() {
+            @Override
+            public void onNext(MessageBrokerProto.Empty value) {
+                // Response received from the server
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // Handle error
+            }
+
+            @Override
+            public void onCompleted() {
+                // Server completed the request
+            }
+        });
+    }
+
+    public void subscribe(String... topics) throws InterruptedException {
+        MessageBrokerProto.Subscription request = MessageBrokerProto.Subscription.newBuilder()
+                .addAllTopics(List.of(topics))
+                .build();
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<MessageBrokerProto.Message> responseObserver = new StreamObserver<MessageBrokerProto.Message>() {
+            @Override
+            public void onNext(MessageBrokerProto.Message value) {
+                // Handle received message
+                System.out.println("Received message: " + value.getContent());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // Handle error
+                latch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                // Server completed the stream
+                latch.countDown();
+            }
+        };
+
+        stub.subscribe(request, responseObserver);
+
+        latch.await(); // Keep the program alive until the subscription is complete
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MessageBrokerClient client = new MessageBrokerClient("localhost", 50051);
+
+        // Publish some messages
+        client.publishMessage("news", "Breaking news: Something happened!");
+        client.publishMessage("politics", "Political update: New laws passed.");
+        client.publishMessage("finance", "Market report: Stocks on the rise.");
+
+        // Subscribe to topics
+        client.subscribe("news", "politics");
+    }
+}
+
---
+++ src/main/java/gRPC/MessageBrokerServer.java
@@ -0,0 +1,82 @@
+package gRPC;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MessageBrokerServer {
+    private final int port;
+    private final Server server;
+
+    private final Map<String, Set<StreamObserver<MessageBrokerProto.Message>>> subscriptions = new HashMap<>();
+
+    public MessageBrokerServer(int port) {
+        this.port = port;
+        this.server = ServerBuilder.forPort(port)
+                .addService(new MessageBrokerImpl())
+                .build();
+    }
+
+    public void start() throws IOException {
+        server.start();
+        System.out.println("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.err.println("Shutting down gRPC server since JVM is shutting down");
+            MessageBrokerServer.this.stop();
+            System.err.println("Server shut down");
+        }));
+    }
+
+    public void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    public void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    private class MessageBrokerImpl extends MessageBrokerGrpc.MessageBrokerImplBase {
+        @Override
+        public void publishMessage(MessageBrokerProto.Message request, StreamObserver<Empty> responseObserver) {
+            String topic = request.getTopic();
+            Set<StreamObserver<MessageBrokerProto.Message>> subscribers = subscriptions.getOrDefault(topic, new HashSet<>());
+            for (StreamObserver<MessageBrokerProto.Message> subscriber : subscribers) {
+                subscriber.onNext(request);
+            }
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        }
+
+        @Override
+        public void subscribe(MessageBrokerProto.Subscription request, StreamObserver<MessageBrokerProto.Message> responseObserver) {
+            Set<StreamObserver<MessageBrokerProto.Message>> subscribers = new HashSet<>();
+            for (String topic : request.getTopicsList()) {
+                subscriptions.computeIfAbsent(topic, k -> new HashSet<>()).add(responseObserver);
+                subscribers.addAll(subscriptions.get(topic));
+            }
+            for (StreamObserver<MessageBrokerProto.Message> subscriber : subscribers) {
+                subscriber.onNext(MessageBrokerProto.Message.newBuilder()
+                        .setTopic("System")
+                        .setContent("New subscriber joined: " + responseObserver)
+                        .build());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        MessageBrokerServer server = new MessageBrokerServer(50051);
+        server.start();
+        server.blockUntilShutdown();
+    }
+}
+
