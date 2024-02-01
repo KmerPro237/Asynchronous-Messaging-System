bc8e2f2104ccad6808b35cdf1eb3a65c787ed237
KmerPro237 <fotsososthene02@gmail.com>
2024-02-01 12:16:46 +0100

    getters and setters implemented

--- Middlewares_Impl/ProtocolTests/JerseyRestClientTest/Employee.java
+++ Middlewares_Impl/ProtocolTests/JerseyRestClientTest/Employee.java
@@ -3,6 +3,27 @@
 public class Employee {
     private int id;
     private String firstName;
-
     // standard getters and setters
+
+
+    public Employee(int id, String firstName) {
+        this.id = id;
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
